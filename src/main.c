2c4ce33476a9cef7c1821cd06f8bf1576b67f08c
Arseney <Arseney300@gmail.com>
2020-03-06 03:09:44 +0700

    update

--- src/main.c
+++ src/main.c
@@ -17,12 +17,31 @@
 
 #define PORT 8080
 #define BUFFER_SIZE 4096
+
+char * hypertext(FILE *file){
+	char *header = "HTTP/1.1 200 OK\r\n\n";
+	char buffer[BUFFER_SIZE];
+	size_t size = strlen(header);
+	char* hypertext_ = malloc(size+1);
+	strcat(hypertext_, header);
+	if(file == NULL){fprintf(stderr, "http_server: problem with file"); exit(EXIT_FAILURE);}
+	while(fgets(buffer, BUFFER_SIZE, file) != NULL){
+		size+=BUFFER_SIZE;
+		hypertext_ = realloc(hypertext_, size);
+		if(hypertext_ == NULL){fprintf(stderr, "http_server: allocating memory error");exit(EXIT_FAILURE);}
+		strcat(hypertext_, buffer);
+	}
+	return hypertext_;
+}
+
+
 int main(int argc, char **argv){
+	FILE* index_file = fopen("examples/index.html","r");
 	int server_fd, new_socket;
 	long valread;
 	struct sockaddr_in address;
 	const int addrlen = sizeof(address);
-	char *message = "HTTP/1.1 200 OK\r\n\n <h3>Hello world</h3>";
+	char *message = hypertext(index_file);
 
 	if((server_fd = socket(AF_INET, SOCK_STREAM,0)) == 0){fprintf(stderr, "http_server: problem with socket");return 1;}
 	address.sin_family = AF_INET;
